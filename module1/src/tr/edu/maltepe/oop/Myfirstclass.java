3d91367af6e3e8063d662697cb5af6b6d5e91f8b
ahmeterenyvs <ahmeteren.metro09@gmail.com>
2025-03-04 15:53:00 +0300

    Update Myfirstclass.java

--- module1/src/tr/edu/maltepe/oop/Myfirstclass.java
+++ module1/src/tr/edu/maltepe/oop/Myfirstclass.java
@@ -1,6 +1,37 @@
 package tr.edu.maltepe.oop;
 
-public class Myfirstclass {
 
+class Class {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Class(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Method to simulate meowing
+    public void meow() {
+        System.out.println(name + " says Meow!");
+    }
 
 }
